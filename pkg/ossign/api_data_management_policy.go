801bc23a5634a3c50eca9c7b947a8e5e487da911
SC Haw <haw@breathelife.com>
2022-07-26 11:39:58 -0400

    Split `api_data_rentention_settings`

--- pkg/ossign/api_data_management_policy.go
+++ pkg/ossign/api_data_management_policy.go
@@ -33,14 +33,6 @@ type DataManagementPolicy struct {
 	TransactionRetention TransactionRetention `json:"transactionRetention"`
 }
 
-type ExpiryTimeConfiguration struct {
-	// Default expiry time for transactions in days
-	Default json.Number `json:"remainingDays"`
-
-	// Maximum allowed value for expiry time for transactions in days
-	Maximum json.Number `json:"maximumRemainingDays"`
-}
-
 func (c *ApiClient) GetDataManagementPolicy() (*DataManagementPolicy, *ApiError) {
 	res, err := c.makeApiRequest("GET", "/api/dataRetentionSettings/dataManagementPolicy", nil)
 
@@ -65,52 +57,6 @@ func (c *ApiClient) GetDataManagementPolicy() (*DataManagementPolicy, *ApiError)
 }
 
 func (c *ApiClient) UpdateDataManagementPolicy(d DataManagementPolicy) *ApiError {
-	body, err := json.Marshal(d)
-
-	if err != nil {
-		return &ApiError{
-			Summary: "unable to marshal the request body",
-			Detail:  err.Error(),
-		}
-	}
-
-	res, apiErr := c.makeApiRequest("PUT", "/api/dataRetentionSettings/dataManagementPolicy", bytes.NewBuffer(body))
-
-	if apiErr != nil {
-		return apiErr
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return getApiError(res)
-	}
-
-	return nil
-}
-
-func (c *ApiClient) GetExpiryTimeConfiguration() (*ExpiryTimeConfiguration, *ApiError) {
-	res, err := c.makeApiRequest("GET", "/api/dataRetentionSettings/expiryTimeConfiguration", nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, getApiError(res)
-	}
-
-	var jsonResp ExpiryTimeConfiguration
-
-	if err := jsonDecode(res.Body, &jsonResp); err != nil {
-		return nil, &ApiError{
-			Summary: "unable to unmarshal the API response",
-			Detail:  err.Error(),
-		}
-	}
-
-	return &jsonResp, nil
-}
-
-func (c *ApiClient) UpdateExpiryTimeConfiguration(d ExpiryTimeConfiguration) *ApiError {
 	body, err := json.Marshal(d)
 
 	if err != nil {
---
+++ pkg/ossign/api_expiry_time_configuration.go
@@ -0,0 +1,61 @@
+package ossign
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type ExpiryTimeConfiguration struct {
+	// Default expiry time for transactions in days
+	Default json.Number `json:"remainingDays"`
+
+	// Maximum allowed value for expiry time for transactions in days
+	Maximum json.Number `json:"maximumRemainingDays"`
+}
+
+func (c *ApiClient) GetExpiryTimeConfiguration() (*ExpiryTimeConfiguration, *ApiError) {
+	res, err := c.makeApiRequest("GET", "/api/dataRetentionSettings/expiryTimeConfiguration", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, getApiError(res)
+	}
+
+	var jsonResp ExpiryTimeConfiguration
+
+	if err := jsonDecode(res.Body, &jsonResp); err != nil {
+		return nil, &ApiError{
+			Summary: "unable to unmarshal the API response",
+			Detail:  err.Error(),
+		}
+	}
+
+	return &jsonResp, nil
+}
+
+func (c *ApiClient) UpdateExpiryTimeConfiguration(d ExpiryTimeConfiguration) *ApiError {
+	body, err := json.Marshal(d)
+
+	if err != nil {
+		return &ApiError{
+			Summary: "unable to marshal the request body",
+			Detail:  err.Error(),
+		}
+	}
+
+	res, apiErr := c.makeApiRequest("PUT", "/api/dataRetentionSettings/dataManagementPolicy", bytes.NewBuffer(body))
+
+	if apiErr != nil {
+		return apiErr
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return getApiError(res)
+	}
+
+	return nil
+}
